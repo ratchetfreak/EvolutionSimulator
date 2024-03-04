636eac430c17bf5daae0f26c9220c1dce416128b
ratchetfreak <ratchet.freak@gmail.com>
2024-03-04 18:32:40 +0100

    get more raii safety

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -7,13 +7,10 @@ FocusableElement *FocusableElement::focusedField;
 agl::Shader		 *MenuShare::menuShader;
 agl::Shader		 *MenuShare::baseShader;
 agl::Camera		 *MenuShare::camera;
-<<<<<<< HEAD
-
 
 std::unique_ptr<agl::Text	  >    MenuShare::text;
 std::unique_ptr<agl::Text	  >    MenuShare::smallText;
 std::unique_ptr<agl::Rectangle > MenuShare::rect;
 std::unique_ptr<agl::Circle	  >  MenuShare::circ;
 std::unique_ptr<agl::Texture	>  MenuShare::border;
-=======
->>>>>>> 804933a (Moved some menu drawing to shader)
+
